86ccea46cf2c6781990f3c2dcab92c798cfea5f0
Hikaru Watanabe <hikaru@paperboy.co.jp>
2018-08-02 10:51:00 +0900

    add uniqueness validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,4 +51,18 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email address should be uniqe" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal @user.email.downcase, @user.reload.email
+  end
+
 end
