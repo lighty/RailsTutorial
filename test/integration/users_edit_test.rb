8f37920b2a91f061e4293b4c3038258afc7e5eb0
Hikaru Watanabe <hikaru@paperboy.co.jp>
2018-08-27 18:09:17 +0900

    add test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -23,6 +23,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as @user
     assert_redirected_to edit_user_path(@user)
+    assert session[:forwarding_url].nil?, 'forwarding not occurs twice'
     name = 'Foo Bar'
     email = 'foo@bar.com'
     patch user_path(@user), params:{ user:{ name: name,
