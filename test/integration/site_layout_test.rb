486bc9eee60eccd95d94668649e44b1fbfb81c4f
Hikaru Watanabe <hikaru@paperboy.co.jp>
2018-08-27 18:37:45 +0900

    users index page can access only loggin user

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,14 +1,33 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layout links' do
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'layout links to not login user' do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count:2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path, count:0
+    assert_select "a[href=?]", logout_path, count:0
     get contact_path
     assert_select 'title', full_title("Contact")
+  end
+
+  test 'layout links to login user' do
+    log_in_as @user
+    get root_path
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
