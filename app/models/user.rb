74fee0fd933691ba056beabb1df1c03a250d7cd6
Hikaru Watanabe <hikaru@paperboy.co.jp>
2018-08-30 23:48:18 +0900

    add activation attributes to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum:255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
@@ -32,5 +33,18 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
+
   end
+
+  private
+  
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest self.activation_token
+    end
+
+    def downcase_email
+      self.email.downcase!
+    end
+
 end
