8869b5fd3a1ece8127879b6af0505c3d81277999
Hikaru Watanabe <hikaru@paperboy.co.jp>
2018-09-05 01:28:06 +0900

    abstract authenticate? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_token.blank?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if token.blank?
+    BCrypt::Password.new(digest).is_password?(token)
   end
   
   def forget
