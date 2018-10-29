74d04e11af9a1245e896d6760208ee8fd78e7bef
lighty <hikalin8686@gmail.com>
2018-10-29 17:28:14 +0900

    dependent destroy

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,4 +80,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
+  test "associated microposts should be destroy" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference "Micropost.count", -1 do
+      @user.destroy
+    end
+  end
+
+
 end
