cccc46731ce31f9fae8356759a824112933c51a8
lighty <hikalin8686@gmail.com>
2018-10-29 19:54:02 +0900

    modify home

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,21 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(micropost_parameters)
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      redirect_to 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def micropost_parameters
+      params.require(:micropost).permit(:content)
+    end
 end
