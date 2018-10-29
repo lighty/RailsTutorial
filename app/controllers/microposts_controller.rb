e1368b4c5580423d598a28ec8b7a61c37bfe6fad
lighty <hikalin8686@gmail.com>
2018-10-29 20:44:06 +0900

    ¤½¤ì¤äݤ¤̿̾¤ËÊѹ¹

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-    @micropost = current_user.microposts.build(micropost_parameters)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'Micropost created!'
       redirect_to root_url
@@ -17,7 +17,7 @@ class MicropostsController < ApplicationController
 
   private
 
-    def micropost_parameters
+    def micropost_params
       params.require(:micropost).permit(:content)
     end
 end
